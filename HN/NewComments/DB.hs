b42c65eec920e3f7347408b73cb7a35045f4fa2b
Anton Nikishaev <me@lelf.lu>
2013-03-19 16:57:21 +0400

    DB: connection, topStored without conn arg
    Ignore-this: 2435a98b12ee8adc6bd5aeb9375f0e38
    
    darcs-hash:20130319125721-055d6-af2723a855a6fda6aa32592ea4f6978dfb74be62

--- HN/NewComments/DB.hs
+++ HN/NewComments/DB.hs
@@ -7,13 +7,19 @@ import Control.Monad
 import HN.NewComments
 import Data.Aeson
 
-topStored :: Connection -> IO Integer
-topStored conn = do [[r]] <- quickQuery conn "select id from comments order by id limit 1" []
-                    return (fromSql r)
+connection ∷ IO Connection
+connection = connectPostgreSQL "dbname=hn"
+
+
+topStored ∷ IO Integer
+topStored = do conn ← connection
+               [[r]] ← quickQuery conn "select id from comments order by id limit 1" []
+               return (fromSql r)
+
 
 storeComments ∷ [Comment] → IO ()
-storeComments comms = do conn ← connectPostgreSQL "dbname=hn"
-                         forM comms (ins conn)
+storeComments comms = do conn ← connection
+                         forM_ comms (ins conn)
                          commit conn
                          disconnect conn
     where ins conn c = run conn "insert into comments (id,data) values (?,?)"
