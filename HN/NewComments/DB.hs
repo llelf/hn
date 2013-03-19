2ccdf7af69a71f1831a1d91b4479bd957818bf11
Anton Nikishaev <me@lelf.lu>
2013-03-19 17:02:38 +0400

    DB.topStored: select hn_id from comments1
    Ignore-this: beddade2c03c64a622109173b3d8c70d
    
    darcs-hash:20130319130238-055d6-73b28b3e7c07f451c6828d3e39df30567355b1c4

--- HN/NewComments/DB.hs
+++ HN/NewComments/DB.hs
@@ -13,7 +13,7 @@ connection = connectPostgreSQL "dbname=hn"
 
 topStored ∷ IO Integer
 topStored = do conn ← connection
-               [[r]] ← quickQuery conn "select id from comments order by id limit 1" []
+               [[r]] ← quickQuery conn "select hn_id from comments1 order by id limit 1" []
                return (fromSql r)
 
 
