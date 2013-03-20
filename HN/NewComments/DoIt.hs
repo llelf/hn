d569cc9ab982b65a6f96ed23b3e178475578826f
Anton Nikishaev <me@lelf.lu>
2013-03-20 10:58:59 +0400

    DoIt: simple syslog
    Ignore-this: b4ea344473b7be76a5b8724ce9e1a6dc
    
    darcs-hash:20130320065859-055d6-584bfa67d5debfb51555485c5399619e55760f63

--- HN/NewComments/DoIt.hs
+++ HN/NewComments/DoIt.hs
@@ -5,14 +5,20 @@ import qualified HN.NewComments.DB as DB
 import Network.HTTP.Conduit
 import qualified Data.ByteString.Lazy.Char8 as BC
 import Data.Time
-
+import Data.Time.Format
+import System.Locale
 
 url = "https://news.ycombinator.com/newcomments"
+
+syslog s = do t <- getCurrentTime
+              appendFile "log" (formatTime defaultTimeLocale "%c" t ++ " " ++ s ++ "\n")
+
 
 storeSome which resp = do now <- getCurrentTime
                           NCPage cc next <- parse now $ BC.unpack resp
                           let news = reverse . filter which $ cc
                           print $ length news
+                          syslog $ "new " ++ show (length news)
                           DB.storeComments news
 
 ini = do resp <- simpleHttp url
