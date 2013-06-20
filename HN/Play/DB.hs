924601abc58ac3f295c3e1960f1a2cbbeafdc175
Anton Nikishaev <me@lelf.lu>
2013-06-20 12:21:34 +0400

    Play.DB: put*,get*
    Ignore-this: d9633563a7e3b7123b325b3c8e8910a6
    
    darcs-hash:20130620082134-055d6-f073dbca32f4ba24ecf9109095ccdbcb54b1aaac

--- HN/Play/DB.hs
+++ HN/Play/DB.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE UnicodeSyntax #-}
-module HN.Play.DB (toSql) where
+{-# LANGUAGE UnicodeSyntax, ScopedTypeVariables #-}
+module HN.Play.DB (connection, getCT,
+                   putC, putS,
+                   module Database.HDBC) where
 
 import Database.HDBC.PostgreSQL
 import Database.HDBC
@@ -15,4 +17,9 @@ query ∷ String → [SqlValue] → IO [[SqlValue]]
 query q xs = connection >>= \c -> quickQuery c q xs
 
 
+
+getCT conn (id :: Integer) = quickQuery conn "select data from thrift1 where id=?" [toSql id]
+
+putC c id d = quickQuery c "insert into comments values (?,?)" [toSql id, toSql d]
+putS c id d = quickQuery c "insert into stories values (?,?)" [toSql id, toSql d]
 
